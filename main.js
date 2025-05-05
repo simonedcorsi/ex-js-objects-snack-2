d52a65cb673298919292a032f97c130b1efa35a3
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-05-05 13:06:21 +0200

    snack 1

---
+++ main.js
@@ -0,0 +1,13 @@
+/* SNACK 1 */
+
+const hamburger = { name: "Cheese Burger", weight: 250 };
+const secondBurger = hamburger;
+secondBurger.name = 'Double Cheese Burger';
+secondBurger.weight = 500;
+
+console.log(hamburger.name); // Double Cheese Burger
+console.log(secondBurger.name); // Double Cheese Burger
+
+// Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? 1
+
